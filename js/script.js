8866b4f4f83efe63c61ed866825a8112d5241e71
Zero <102381@glr.nl>
2025-02-07 14:12:13 +0000

    muted song

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ document.getElementById("charC").addEventListener("click", function () {
 
 function changeVideo(videoSrc) {
     let videoPlayer = document.getElementById("videoplayer");
-    videoPlayer.innerHTML = `<video controls autoplay width="100%" height="auto">
+    videoPlayer.innerHTML = `<video controls muted autoplay width="100%" height="auto">
                                 <source src="${videoSrc}" type="video/mp4">
                                 Your browser does not support the video tag.
                              </video>`;
